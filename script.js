bfccbc951d913d1fac6e3dda23d90ac9b5bf66f0
andrewleachtx <andrewleach38@gmail.com>
2024-07-04 15:10:31 -0500

    Reverted to old colors, changed vocab, and added color coding

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Target the menu and icon elements, toggle the .open CSS class
 function toggleMenu() {
-    // Grab the first instance of class '.menu-links'
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  // Grab the first instance of class '.menu-links'
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
