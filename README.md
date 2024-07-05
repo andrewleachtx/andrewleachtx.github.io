88d47dcd0fc470b1e0b0ca1e1320e2e428123a31
andrewleachtx <andrewleach38@gmail.com>
2024-07-04 20:42:47 -0500

    Experience color underlining, mathJAX support added, JS template code for project listing done, file structure to allow for project HTML

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Personal Website
 
-Templated entirely off of this guide:
+1. Style and theme based off of this [tutorial](https://www.youtube.com/watch?v=ldwlOzRvYOU).
 
-https://www.youtube.com/watch?v=ldwlOzRvYOU
+2. [MathJax](https://docs.mathjax.org/en/latest/basic/mathematics.html) has two main points:
+   1. **Inline**: `\( ... \)`
+   2. **Standalone/Display**: `$$ ... $$`
---
+++ projects.js
@@ -0,0 +1,51 @@
+const projects = [
+  {
+    title: "Ray Tracer",
+    date: "April 2024",
+    image: "../projects/raytracer/scene.png",
+    link: "../projects/raytracer/raytracer.html",
+    desc: "Ray tracing engine featuring constructive solid geometry, anti-aliasing, multithreading, and 9 scenes.",
+  },
+  {
+    title: "TBD.",
+    date: "January 0000",
+    image: "",
+    link: "",
+    desc: "More will be added to this doc soon",
+  }
+  //   {
+  //     title: "Deferred Renderer",
+  //     date: "March 2024",
+  //     image: "./projects/"
+  //   }
+];
+
+// Just a sort w/ comparator, for now orders top to bottom newest to oldest
+function sortProjects(projects) {
+  return projects.sort((a, b) => new Date(b.date) - new Date(a.date));
+}
+
+function genProjects() {
+  // Get big container for all of it
+  const container = document.querySelector(".projects-container");
+  const projects_ordered = sortProjects(projects);
+
+  projects_ordered.forEach((proj) => {
+    const proj_ele = document.createElement("div");
+
+    proj_ele.classList.add("project-card");
+    proj_ele.innerHTML = `
+        <a href="${proj.link}">
+            <img class="project-image" alt="${proj.title} Image" src="${proj.image}" />
+            <div class="project-details">
+                <h3 class="project-title">${proj.title}</h3>
+                <p class="project-date">${proj.date}</p>
+                <p class="project-desc">${proj.desc}</p>
+            </div>
+        </a>
+    `;
+    container.appendChild(proj_ele);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", genProjects);
