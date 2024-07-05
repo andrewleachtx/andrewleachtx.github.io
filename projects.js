89aa6e2e3b8c3d503f1293e28cc19a15c65b11d9
andrewleachtx <andrewleach38@gmail.com>
2024-07-05 15:39:50 -0500

    Added site to projects; added margin to pages/projects.html

--- projects.js
+++ projects.js
@@ -2,17 +2,27 @@ const projects = [
   {
     title: "Ray Tracer",
     date: "April 2024",
+    timeframe: "1 month",
     image: "../projects/raytracer/assets/scene.png",
     link: "../projects/raytracer/raytracer.html",
     desc: "Ray tracing engine featuring constructive solid geometry, anti-aliasing, multithreading, and 9 scenes.",
   },
   {
+    title: "Personal Website",
+    date: "June 2024",
+    timeframe: "Ongoing",
+    image: "../assets/site.png",
+    link: "../index.html",
+    desc: "This website - built on native HTML, CSS, and JavaScript.",
+  },
+  {
     title: "TBD...",
     date: "January 0000",
+    timeframe: "",
     image: "",
     link: "",
-    desc: "More will be added to this doc soon",
-  }
+    desc: "Many more projects have yet to be migrated to this site.",
+  },
   //   {
   //     title: "Deferred Renderer",
   //     date: "March 2024",
@@ -39,7 +49,7 @@ function genProjects() {
             <img class="project-image" alt="${proj.title} Image" src="${proj.image}" />
             <div class="project-details">
                 <h3 class="project-title">${proj.title}</h3>
-                <p class="project-date">${proj.date}</p>
+                <p class="project-date">${proj.date} • ${proj.timeframe}</p>
                 <p class="project-desc">${proj.desc}</p>
             </div>
         </a>
